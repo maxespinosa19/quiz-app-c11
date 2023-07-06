a9c4fdbe83305e7d4598fc8ec4cc9188fb2a5899
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-07-06 16:29:23 -0400

    style change

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -2,10 +2,10 @@ import Footer from "./Footer"
 import NavMenu from "./NavMenu/NavMenu"
 export default function AppLayout({ children }){
     return(
-        <>
+        <div id="main-container">
         {/* <NavMenu/> */}
         {children}
         <Footer/>
-        </>
+        </div>
     )
 }
